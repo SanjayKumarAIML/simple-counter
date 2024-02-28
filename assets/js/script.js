331274aa16ac333354f138cb7c84830c1cc332b6
SanjayKumarAIML <sanjay2003aug10@gmail.com>
2024-02-28 08:17:09 +0530

    Add - script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
+let counterValue = 0
+const counterDisplay = document.getElementById("counter-number");
+
 const incrementFunction = () => {
+    counterValue++
+    updateDisplay()
     console.log(`increment logged!`)
 }
 
 const decrementFunction = () => {
+    counterValue--
+    updateDisplay()
     console.log(`decrement logged!`)
+}
+
+function updateDisplay(){
+    counterDisplay.textContent = counterValue
 }
